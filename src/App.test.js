b494b5497d70196beefc8b2d614a9a27e84a63b3
dytsou <d.y.tsou@gmail.com>
2025-11-10 18:41:11 +0800

    fix(tests): Remove duplicate import of App component in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import App from './App';
 jest.mock('./logo.svg', () => 'logo.svg');
 jest.mock('./App.css', () => '');
-import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
